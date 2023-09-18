883f37d5594d97f6017cb630e3e4a5ae6a68a80c
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-09-18 14:36:42 -0300

    Feat: Scripts, rotas de usuario e controller

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express")
-
 const ConnectToDatabase = require("./src/database/database")
+const usuario = require("./src/router/usuario.router")
 
 const app = express()
 
 const port = 5000
 
+app.use(express.json())
+
 ConnectToDatabase()
 
-app.use(express.json())
+app.use("/usuario", usuario)
 
 app.get("/", (req,res) => {
     res.send({
---
+++ src/router/usuario.router.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const usuarioController = require("../controller/usuario.controller")
+
+router.get('/findById:id');
+router.get('/findAll');
+
+router.post('/create');
+router.post('/addAddres/:id');
+router.post('/addFavProduct/:id');
+
+router.put('/update/:id');
+
+router.delete('/remove/:id');
+router.delete('/removeAdress');
+router.delete('/removeFavProduct');
+
+module.exports = router
