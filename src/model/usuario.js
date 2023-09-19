b52f368be6987325bb6737e2027f36f76b50a1dd
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-09-19 13:33:53 -0300

    corrigindo sintaxe

--- src/model/usuario.js
+++ src/model/usuario.js
@@ -1,24 +1,24 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const usuarioSchema = new mongoose.Schema({
-    nome:{type: String, required:true},
-    email:{ype: String, unique:true, required: true},
-    senha:{type: String, required:true},
-    imagem:{type: String, required:true},
-    enderecos:[{
-        rua: {type: String, required:true},
-        numero:{type: Number, required:true},
-        CEP: {type: String, required: true},
-        createdAt: {type: Date, required:true},
+    nome: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    senha: { type: String, required: true },
+    imagem: { type: String, required: true },
+    enderecos: [{
+        rua: { type: String, required: true },
+        numero: { type: Number, required: true },
+        CEP: { type: String, required: true },
+        createdAt: { type: Date, required: true },
     }],
-    createAt: {createdAt: {type: Date, required:true}},
+    createdAt: { type: Date, required: true }, // Corrigido o nome do campo
     produtos_fav: [{
-        _id: {type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref:"produtos"},
-        createdAt: {type: Date, required:true},
+        _id: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref: "produtos" },
+        createdAt: { type: Date, required: true },
     }],
-    admin:{type: Boolean, required:true, default: false},
-})
+    admin: { type: Boolean, required: true, default: false },
+});
 
-const Usuario = mongoose.model("usuarios", usuarioSchema)
+const Usuario = mongoose.model("usuarios", usuarioSchema);
 
-module.exports = Usuario
+module.exports = Usuario;
